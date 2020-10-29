84b557d6343edbe35d12d785d9e28663f73c2342
Aleksandr Tikhonov <aleksandr.tikhonov@sap.com>
2020-10-29 20:06:56 +0100

    init commit

---
+++ ladder/ladder/ladderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ladderApp.swift
+//  ladder
+//
+//  Created by Tikhonov, Aleksandr on 29.10.20.
+//
+
+import SwiftUI
+
+@main
+struct ladderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
